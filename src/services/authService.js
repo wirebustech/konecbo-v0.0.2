768599de86f6fe90b418344866fe97b181c792cc
Oputa Uche <wirebuslab@gmail.com>
2026-01-20 00:41:03 -0400

    fix(frontend): use dynamic API URL based on window.location

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : 'https://konecbo-main.azurewebsites.net/api';
 
 // Create axios instance with default config
 const api = axios.create({
